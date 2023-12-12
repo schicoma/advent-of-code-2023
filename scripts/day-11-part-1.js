3565a0ec2e8c919c3ef04f5bcf6d90db7bbd3677
Sebastian Chicoma Sandmann <sebastianchicoma97@gmail.com>
2023-12-13 00:37:27 +0100

    Day 11 part 1 and 2

---
+++ scripts/day-11-part-1.js
@@ -0,0 +1,95 @@
+/**
+ *
+ * @param {string[]} inputs
+ */
+const process = (inputs) => {
+  let galaxies = []
+  let emptyRows = []
+  let emptyColumns = []
+
+  for (let row = 0; row < inputs.length; row++) {
+    const galaxiesInRowMatches = inputs[row].matchAll(/[#]/g)
+    const galaxiesInRow = Array.from(galaxiesInRowMatches)
+
+    // evaluation row
+    if (!galaxiesInRow.length) {
+      emptyRows.push(row)
+    }
+
+    for (let galaxy of galaxiesInRow) {
+      galaxies.push([row, galaxy.index])
+    }
+
+    // evaluating column
+    let areGalaxiesInColumn = false
+    for (let pointer = 0; pointer < inputs.length; pointer++) {
+      // second conditions is to avoid duplicates
+      if (inputs[pointer][row] === '#') {
+        areGalaxiesInColumn = true
+      }
+    }
+
+    if (!areGalaxiesInColumn) {
+      emptyColumns.push(row)
+    }
+  }
+
+  // console.log(galaxies, emptyRows, emptyColumns)
+
+  const memo = {}
+
+  let totalPath = 0
+
+  for (let i = 0; i < galaxies.length; i++) {
+    const from = galaxies[i]
+    for (let j = i + 1; j < galaxies.length; j++) {
+      const to = galaxies[j]
+      let jumpsRow = 0
+      let jumpsCols = 0
+
+      for (let index of emptyRows) {
+        if (
+          Math.min(from[0], to[0]) < index &&
+          index < Math.max(from[0], to[0])
+        ) {
+          jumpsRow++
+        }
+      }
+
+      for (let index of emptyColumns) {
+        if (
+          Math.min(from[1], to[1]) < index &&
+          index < Math.max(from[1], to[1])
+        ) {
+          jumpsCols++
+        }
+      }
+
+      const path = Math.abs(to[0] - from[0]) + Math.abs(to[1] - from[1])
+
+      // console.log(from, to, path, '|', jumpsRow, jumpsCols)
+      totalPath += path + jumpsRow + jumpsCols
+    }
+  }
+
+  return totalPath
+}
+
+module.exports = {
+  process,
+}
+
+/**
+
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+
+ */
